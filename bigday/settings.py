31be4af094f8852597aefee491c7e6305e76da9b
elamnoj <male.jonathan@gmail.com>
2022-11-20 17:35:15 -0600

    sdfg

--- bigday/settings.py
+++ bigday/settings.py
@@ -51,7 +51,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'templates'),
+            os.path.join('weddingsite/bigday', 'templates'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
