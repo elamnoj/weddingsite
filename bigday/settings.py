15bb9dd8348044635c8cc3c33e1b89d1c5987145
elamnoj <male.jonathan@gmail.com>
2022-11-20 19:44:43 -0600

    need static files

--- bigday/settings.py
+++ bigday/settings.py
@@ -117,8 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_ROOT = 'static_root'
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = (
+    BASE_DIR,
+    'static',
     os.path.join(BASE_DIR, 'static_root'),
 )
 
